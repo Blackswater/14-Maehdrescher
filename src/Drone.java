1f856ecae7970bcc7ea0b21925a52088af182509
Raphael Lawo <RaphaelLawo@gmx.de>
2019-01-09 08:31:21 +0100

    final

--- src/Drone.java
+++ src/Drone.java
@@ -8,7 +8,7 @@ public class Drone implements IDrone {
     public Map<String, Wheat> scanField() {
         Map<String, Wheat> wheatHashSet = new HashMap<>();
         for (int i = 0; i <= Field.getField().length; i++) {
-            for (int j = 0; j <= Field.getField().length; j++) {
+            for (int j = 0; j <= Field.getField()[0].length; j++) {
 
                 wheatHashSet.put(String.valueOf(0)+String.valueOf(i)+String.valueOf(j),new Wheat(i,j));
             }
